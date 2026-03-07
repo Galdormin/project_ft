a1e8ce27f555f1b4abc5d263ab8e970e7d7a7aed
Galdormin <theoplenet@gmail.com>
2026-03-07 16:41:29 +0100

    remove test_get_files_recursive (break CI/CD)

--- tests/unit/test_utils.gd
+++ tests/unit/test_utils.gd
@@ -6,7 +6,3 @@ func test_assert_subset():
     assert_eq(TestUtils.subset_of(["Titi", "Tata"], ["Titi", "Tata", "Toto"]), true)
     
     assert_eq(TestUtils.subset_of(["Titi", "Tata", "TuTu"], ["Titi", "Tata", "Toto"]), false)
-
-func test_get_files_recursive():
-    var files = TestUtils.get_files_recursive("res://", "json")
-    assert_eq(len(files), 3)
